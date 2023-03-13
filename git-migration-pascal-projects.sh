df3498d0edf66699e201d5e24a0b0558cd48caa5
Lothar Behrens <lothar.behrens@lollisoft.de>
2023-03-14 00:25:53 +0100

    Changed uri to CVS repo

--- git-migration-pascal-projects.sh
+++ git-migration-pascal-projects.sh
@@ -3,7 +3,7 @@
 mkdir cvs_migration_pascal
 
 cd cvs_migration_pascal/
-git cvsimport -v -d :pserver:anonymous@lbdmf.cvs.sourceforge.net:/cvsroot/lbdmf PASCAL
+git cvsimport -v -d :pserver:lothar@localhost:/home/cvsroot PASCAL
 git remote add origin git@github.com:lollisoft/pascal.git
 git branch -M master
 git push -u origin master
