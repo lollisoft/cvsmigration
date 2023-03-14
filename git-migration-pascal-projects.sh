d40ca56607aeaccf78c2ece6e974a9da3416715a
Lothar Behrens <lothar@localhost.localdomain>
2023-03-14 04:08:50 +0100

    Changes that let this migration script work.

--- git-migration-pascal-projects.sh
+++ git-migration-pascal-projects.sh
@@ -1,11 +1,29 @@
 #!/bin/sh
 
-mkdir cvs_migration_pascal
+rm -rf pascal
 
-cd cvs_migration_pascal/
-git cvsimport -v -d :pserver:lothar@localhost:/home/cvsroot PASCAL
-git remote add origin git@github.com:lollisoft/pascal.git
-git branch -M master
-git push -u origin master
-git push origin --tags
+# Get an empty git repo and create an initial file in the very past before any cvs timestamps
+git clone git@github.com:lollisoft/pascal.git 
+cd pascal
+echo "This is a migration from my old CVS PASCAL repository." >> README.md
+git add README.md
+git commit -m "First commit" --date="1980-01-01 00:00:00"
+git branch -M main
+git push
 cd ..
+
+
+rm -rf project.cvs
+mkdir project.cvs    
+cd pascal 
+cd ../project.cvs
+git cvsimport -o main -C ../pascal -k -d :pserver:lothar@cvsserver.behrens.de:/home/cvsroot PASCAL
+cd ..
+cd pascal
+git branch -M main
+git push
+git push origin --tags
+echo Cleanup
+rm -rf pascal
+rm -rf project.cvs
+echo I am ready
